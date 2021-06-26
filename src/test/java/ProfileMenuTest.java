ea3833ed030a4f9ea5a7dcdb72d822fcd614f3f9
ParasEnayati <82240214+ParasEnayati@users.noreply.github.com>
2021-06-26 14:02:01 +0430

    Fixed Profile Test

--- src/test/java/ProfileMenuTest.java
+++ src/test/java/ProfileMenuTest.java
@@ -16,15 +16,26 @@ public class ProfileMenuTest {
                 menu enter Profile
                 sad
                 profile change -p -c x -n y
+                profile change -p -n z -c y
                 profile change -p -c x -n y
+                profile change -p -c z -n z
                 profile change -n mamad
+                profile change -n mamad
+                profile change -n mohsen
+                profile change -u ali
                 profile change -u ali
                 menu enter Shop
                 menu show-current
+                profile change -c a -p -n b
+                profile change -c b -n a -p
+                profile change -n q -p -c a
+                profilechange -n s -c q -p
+                profile change -n s -c q -p
                 menu exit
-                user logout
                 menu show-current
-                menu exit""").getBytes());
+                user logout
+                menu exit
+                """).getBytes());
         System.setIn(in);
         new ProgramController().run();
         Assert.assertEquals("""
@@ -32,13 +43,23 @@ public class ProfileMenuTest {
                 user logged in successfully!\r
                 invalid command\r
                 password changed successfully!\r
+                password changed successfully!\r
                 current password is invalid\r
+                please enter a new password\r
+                nickname changed successfully!\r
+                user with nickname mamad already exists\r
                 nickname changed successfully!\r
                 username changed successfully!\r
+                user with username ali already exists\r
                 menu navigation is not possible\r
                 Profile\r
-                user logged out successfully!\r
-                Login Menu\r
+                current password is invalid\r
+                current password is invalid\r
+                current password is invalid\r
+                invalid command\r
+                current password is invalid\r
+                Main Menu\r
+                user logged out successfully!\r                               
                 """, outContent.toString());
         System.setIn(sysInBackup);
     }
