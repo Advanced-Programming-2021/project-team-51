d35506328a9d911bd5c5686f6636f52475f11d34
ParasEnayati <82240214+ParasEnayati@users.noreply.github.com>
2021-07-12 03:54:32 +0430

    Add Trailer To Game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import controller.GUI.LoginControllerGUI;
+import controller.GUI.SceneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import models.Deck;
 import models.User;
@@ -12,19 +16,24 @@ import models.cards.Card;
 import models.cards.MakeCards;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main extends Application {
     public static Stage primaryStage;
+    public static String mp4File = "./src/main/resources/video/trailer.mp4";
+    public static Media media = new Media(new File(mp4File).toURI().toString());
+    MediaPlayer mediaPlayer1 = new MediaPlayer(media);
 
     @Override
     public void start(Stage stage) throws Exception {
-        String musicFile = "./src/main/resources/sound/main.mp3";
-        Media sound = new Media(new File(musicFile).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(sound);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        LoginControllerGUI.player = mediaPlayer;
-        mediaPlayer.play();
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/start.fxml"));
+
+        MediaView mediaView = new MediaView(mediaPlayer1);
+        mediaPlayer1.play();
+        mediaView.setFitHeight(600);
+        mediaView.setFitWidth(800);
+
+        Pane root = FXMLLoader.load(getClass().getResource("/fxml/pre_start.fxml"));
+        root.getChildren().add(mediaView);
         stage.setTitle("Yu Gi Oh");
         stage.setScene(new Scene(root));
         primaryStage = stage;
@@ -60,5 +69,16 @@ public class Main extends Application {
             mamad.getUserDeckByName("s").addCardToDeck(true,cards);
         }
         mamad.setActiveDeck(mamad.getUserDeckByName("s"));
+    }
+
+    public void goToStartScene(MouseEvent event) throws IOException {
+        mediaPlayer1.setVolume(0);
+        String musicFile = "./src/main/resources/sound/main.mp3";
+        Media sound = new Media(new File(musicFile).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        LoginControllerGUI.player = mediaPlayer;
+        mediaPlayer.play();
+        SceneController.switchSceneMouse("/fxml/start.fxml",event);
     }
 }
