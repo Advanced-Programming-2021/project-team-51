0cb0614a0306c77a2f059a6d6ebf7fea86f4a615
ParasEnayati <82240214+ParasEnayati@users.noreply.github.com>
2021-07-11 12:01:53 +0430

    Setting Scene

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import models.Deck;
 import models.User;
 import models.cards.Card;
+import models.cards.MakeCards;
 
 import java.io.File;
 
@@ -31,21 +32,22 @@ public class Main extends Application {
     }
     public static void main(String[] args) {
         User mamad = new User("a","a","a");
-        mamad.addCard(Card.getCardByName("Magic Jammer"));
-        mamad.addCard(Card.getCardByName("Magic Jammer"));
-        mamad.addCard(Card.getCardByName("Magic Jammer"));
+        mamad.addCard(MakeCards.makeCard("Magic Jammer"));
+        mamad.addCard(MakeCards.makeCard("Magic Jammer"));
+        mamad.addCard(MakeCards.makeCard("Magic Jammer"));
 
-        mamad.addCard(Card.getCardByName("Battle OX"));
-        mamad.addCard(Card.getCardByName("Battle OX"));
-        mamad.addCard(Card.getCardByName("Battle OX"));
+        mamad.addCard(MakeCards.makeCard("Battle OX"));
+        mamad.addCard(MakeCards.makeCard("Battle OX"));
+        mamad.addCard(MakeCards.makeCard("Battle OX"));
 
-        mamad.addCard(Card.getCardByName("Suijin"));
-        mamad.addCard(Card.getCardByName("Suijin"));
-        mamad.addCard(Card.getCardByName("Suijin"));
+        mamad.addCard(MakeCards.makeCard("Suijin"));
+        mamad.addCard(MakeCards.makeCard("Suijin"));
+        mamad.addCard(MakeCards.makeCard("Suijin"));
 
-        mamad.addCard(Card.getCardByName("Bitron"));
-        mamad.addCard(Card.getCardByName("Bitron"));
-        mamad.addCard(Card.getCardByName("Bitron"));
+
+        mamad.addCard(MakeCards.makeCard("Bitron"));
+        mamad.addCard(MakeCards.makeCard("Bitron"));
+        mamad.addCard(MakeCards.makeCard("Bitron"));
 
         mamad.addDeck(new Deck("s","a"));
         for (Card cards: mamad.getUserCards()
